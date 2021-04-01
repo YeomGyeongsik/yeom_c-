946a7d4211e55af7ada78c1c3b02f03c359dad13
YeomGyeongsik <80300334+YeomGyeongsik@users.noreply.github.com>
2021-04-01 23:55:59 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-k
+class Car {
+	const string model;
+	bool power;
+	double speed;
+
+public:
+	Car(string m, bool p = false, double s = 0.0) :model(m), power(p), speed(s) {};
+	void set_power();
+	void set_speed(bool);
+	double get_speed() const;
+};
+void Car::set_power()
+{
+	power = !power;
+	return;
+}
+void Car::set_speed(bool a) {
+	if (!power) return;
+	if (a)
+		speed += 1000;
+	else
+		speed -= 10;
+	return;
+}
+double Car::get_speed()const
+{
+	return speed;
+}
+  
