45827fcf1eea8361af845fc6cb42912b71325297
YeomGyeongsik <80300334+YeomGyeongsik@users.noreply.github.com>
2021-04-11 16:28:13 +0900

    Update dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 using namespace std;
-
-
-class Animal{
+class Animal {
 public:
-virtual void Cry() = 0;
+	virtual void Cry() = 0;
 };
-class Dog : public Animal{
+class Cat : public Animal {
 public:
-void Cry(){
-cout << "멍멍\n"; return; }
+	void Cry() {
+		cout << "냥냥\n"; return;
+	}
 };
 
 
-class Bird : public Animal{
+class Dog : public Animal {
 public:
-void Cry(){
-cout << "짹짹\n"; return; };
+	void Cry() {
+		cout << "멍멍\n"; return;
+	}
+};
+class Bird : public Animal {
+public:
+	void Cry() {
+		cout << "짹짹\n"; return;
+	};
 };
 int main()
 {
-Animal *pa = new Dog;
-Animal *pb = new Bird;
-pa->Cry();
-pb->Cry();
-Dog *pd = (Dog *) pa;
-pd->Cry();
-return 0;
+	Animal* pa = new Cat;
+	Animal* pb = new Dog;
+	Animal* pc = new Bird;
+	pa->Cry();
+	pb->Cry();
+	pc->Cry();
+	Dog* pd = (Dog*)pa;
+	pd->Cry();
+	return 0;
 }
