c794a6a49119309edeca6c250523cf0ac6443de3
YeomGyeongsik <80300334+YeomGyeongsik@users.noreply.github.com>
2021-04-11 16:23:48 +0900

    Update dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -1,1 +1,30 @@
-1
+#include <iostream>
+using namespace std;
+
+
+class Animal{
+public:
+virtual void Cry() = 0;
+};
+class Dog : public Animal{
+public:
+void Cry(){
+cout << "멍멍\n"; return; }
+};
+
+
+class Bird : public Animal{
+public:
+void Cry(){
+cout << "짹짹\n"; return; };
+};
+int main()
+{
+Animal *pa = new Dog;
+Animal *pb = new Bird;
+pa->Cry();
+pb->Cry();
+Dog *pd = (Dog *) pa;
+pd->Cry();
+return 0;
+}
